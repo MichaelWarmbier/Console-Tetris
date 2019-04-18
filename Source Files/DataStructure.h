6408b9da439b0f9867a24b1b0a2d3d92cee0332c
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-04-18 00:26:55 -0400

    - Initialized DataStructure.h

---
+++ Source Files/DataStructure.h
@@ -0,0 +1,9 @@
+#pragma once
+// Arrays
+const char logo[5][22]{
+	"ttt eee ttt rrr i sss",
+	" t  e    t  r r   s  ",
+	" t  eee  t  rr  i  s ",
+	" t  e    t  r r i   s",
+	" t  eee  t  r r i sss",
+};
