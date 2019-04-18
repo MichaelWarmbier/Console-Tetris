175e187a543cc70bd5182d396a05351711ae3e46
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-04-18 01:09:51 -0400

    - Added newest function prototypes and comments

--- Source Files/Main.cpp
+++ Source Files/Main.cpp
@@ -7,8 +7,14 @@ void MainMenuDraw();
 void MainMenuInput();
 void MainMenuLogic();
 
+void SetMenuBounds(); // Logic function reserved for mainly bounds
+void OutputSelectionArrow(int); // Outputs selection arrow with proper formating with argument of menu position
+
 // Function Prototypes
-void drawLogo(int, int);
+void drawLogo(int, int); // Draws logo at position x,y, based off argument
+void indent(int); // Indents based off argument
+char getColor(int); // Returns a character based off an argument, for updating logo colors
+void ShowConsoleCursor(bool); // Shows or hides cursor based off bool
 
 #include "Implementation.h"
 
