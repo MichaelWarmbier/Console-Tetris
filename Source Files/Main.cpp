4ed115021f78bccff89c75a718851d5219a75356
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-04-22 00:50:25 -0400

    - Added Pause/Start functionality and prototypes

--- Source Files/Main.cpp
+++ Source Files/Main.cpp
@@ -7,8 +7,10 @@ void MainMenuDraw();
 void MainMenuInput();
 void MainMenuLogic();
 
-void SetMenuBounds(); // Logic function reserved for mainly bounds
-void OutputOption(int, const char[10]); // Outputs selection arrow with proper formating with argument of menu position and option name
+void TetrisSetup();
+void TetrisDraw();
+void TetrisInput();
+void TetrisLogic();
 
 // Function Prototypes
 void drawLogo(int, int); // Draws logo at position x,y, based off argument
@@ -17,6 +19,11 @@ char getColor(int); // Returns a character based off an argument, for updating l
 void ShowConsoleCursor(bool); // Shows or hides cursor based off bool
 void DrawHowToPlay(); // Draws How To Play portion of screen
 void newLine(int, int); // Resets cursor position to X argument, and adds Y argument
+void SetMenuBounds(); // Logic function reserved for mainly bounds
+void OutputOption(int, const char[10]); // Outputs selection arrow with proper formating with argument of menu position and option name
+
+void DrawBoard(int, int);
+void DrawStatistics(int, int);
 
 #include "Implementation.h"
 
@@ -28,8 +35,13 @@ int main() {
 			MainMenuInput();
 			MainMenuLogic();
 		} while (!EXIT_MENU);
+		if (!EXIT_TETRIS)
+			TetrisSetup();
 		while (!EXIT_TETRIS) { // Tetris Loop
-
+			TetrisDraw();
+			TetrisInput();
+			if (!pause && game_start)
+				TetrisLogic();
 		}
 	} while (!EXIT_PROGRAM);
 	return 0;
