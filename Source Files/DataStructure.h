2230cb939391e01c5a2c0ec6333aca023d8d2c91
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-04-23 00:59:52 -0400

    - Board array updates

--- Source Files/DataStructure.h
+++ Source Files/DataStructure.h
@@ -1,5 +1,13 @@
 #pragma once
+//////////////
+// Enumerators
+//////////////
+enum input {RL, RR, UP, DOWN, LEFT, RIGHT, ENTER, BACK, NONE};
+enum state {BEFORE, DURING, AFTER, LIMBO};
+
+/////////
 // Arrays
+/////////
 const char logo[5][22]{
 	"ttt eee TTT rrr i sss",
 	" t  e    T  r r   s  ",
@@ -7,8 +15,8 @@ const char logo[5][22]{
 	" t  e    T  r r i   s",
 	" t  eee  T  r r i sss",
 };
-char tetris_board[20][21] {
-	"####################",
+char tetris_board[20][21]{
+	"#**********#########",
 	"#          # Next: #",
 	"#          #   b   #",
 	"#          #########",
@@ -30,7 +38,7 @@ char tetris_board[20][21] {
 	"############",
 };
 char tetris_board_copy[20][21]{
-	"####################",
+	"#**********#########",
 	"#          # Next: #",
 	"#          #   b   #",
 	"#          #########",
