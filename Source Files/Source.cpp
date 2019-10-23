db073f61ee0507cc6ecc9f198899a948aa8c1ca8
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-10-23 19:34:19 -0400

    Update Source.cpp

--- Source Files/Source.cpp
+++ Source Files/Source.cpp
@@ -3,6 +3,7 @@
 /* Main */
 
 int main() {
+	SetWindowDimensions(WW + 1,WH + 3);
 	do {
 		Game * Tetris = new Game;
 		while (!EXIT_GAME_F) {
@@ -35,4 +36,45 @@ double Wait(double waitTime) {
 	while (waitTime > GetTimeSince(startTime)) {}
 
 	return GetTimeSince(startTime + waitTime);
+}
+
+void SetWindowDimensions(int x, int y) { // Thanks for this, Sam!
+	HWND console = GetConsoleWindow();
+	HMONITOR monitor = MonitorFromWindow(console, MONITOR_DEFAULTTOPRIMARY);
+
+	MONITORINFOEX miex;
+	miex.cbSize = sizeof(miex);
+	GetMonitorInfo(monitor, &miex);
+	int cxLogical = (miex.rcMonitor.right - miex.rcMonitor.left);
+	int cyLogical = (miex.rcMonitor.bottom - miex.rcMonitor.top);
+
+	DEVMODE dm;
+	dm.dmSize = sizeof(dm);
+	dm.dmDriverExtra = 0;
+	EnumDisplaySettings(miex.szDevice, ENUM_CURRENT_SETTINGS, &dm);
+	int cxPhysical = dm.dmPelsWidth;
+	int cyPhysical = dm.dmPelsHeight;
+
+	double horzScale = ((double)cxPhysical / (double)cxLogical);
+	double vertScale = ((double)cyPhysical / (double)cyLogical);
+	SetWindowPos(console, HWND_TOP, 0, 0, x * 16.0 / horzScale, y * 16.0 / vertScale, SWP_NOMOVE);
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(handle, &info);
+	COORD new_size = {
+		info.srWindow.Right - info.srWindow.Left + 1,
+		info.srWindow.Bottom - info.srWindow.Top + 1
+	};
+	SetConsoleScreenBufferSize(handle, new_size);
+	ShowConsoleCursor(false);
+}
+
+void ShowConsoleCursor(bool showFlag) {
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_CURSOR_INFO     cursorInfo;
+
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = showFlag;
+	SetConsoleCursorInfo(out, &cursorInfo);
 }
