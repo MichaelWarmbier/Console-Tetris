b56c1a7ee4adaf0b2372bf565ee26b803f09d640
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-10-29 18:10:42 -0400

    Update Header.h

--- Source Files/Header.h
+++ Source Files/Header.h
@@ -24,7 +24,7 @@ const HDC hdc = CreateCompatibleDC(NULL); // Handle for device context for bitma
 const HBITMAP bmap = (HBITMAP)LoadImage(NULL, _T("ConsoleTetrisSpriteSheet.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE); // Bitmap image data
 
 enum GameState {BEFORE, DURING, LIMBO, AFTER};
-enum Direction {UP, DOWN, LEFT, RIGHT, NONE, RL, RR};
+enum Direction {UP, DOWN, LEFT, RIGHT, NONE, RL, RR, ENTER};
 enum BlockType {bO = 0, bI = 1, bT = 2, bL = 3, bJ = 4, bS = 5, bZ = 6, bX = 7};
 
 const int GSS = 16; // Global Sprite Size
