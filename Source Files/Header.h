debd364e952b7e83990d9341d70cadab2af86873
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-10-29 14:46:08 -0400

    Update Header.h

--- Source Files/Header.h
+++ Source Files/Header.h
@@ -11,12 +11,11 @@ using namespace chrono;
 static random_device generator;
 static uniform_int_distribution<int> distribution(0, 6);
 
-/* Debug Options */
-static bool ForceSpriteTest = false;
-
-/* Exit Flags */
 static bool EXIT_GAME_F = false;
 static bool EXIT_PROGRAM_F = false;
+
+/* Debug Options */
+static bool ForceSpriteTest = false;
 
 typedef unsigned char byteint; // Integer with range of one byte
 
@@ -24,7 +23,7 @@ const HDC console = GetDC(GetConsoleWindow()); // Handle for device context spec
 const HDC hdc = CreateCompatibleDC(NULL); // Handle for device context for bitmap
 const HBITMAP bmap = (HBITMAP)LoadImage(NULL, _T("ConsoleTetrisSpriteSheet.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE); // Bitmap image data
 
-enum GameState {BEFORE, DURING, LIMBO, WIN, LOSS};
+enum GameState {BEFORE, DURING, LIMBO, AFTER};
 enum Direction {UP, DOWN, LEFT, RIGHT, NONE, RL, RR};
 enum BlockType {bO = 0, bI = 1, bT = 2, bL = 3, bJ = 4, bS = 5, bZ = 6, bX = 7};
 
